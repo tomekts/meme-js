9508c68f7674dfb88c1026b9d583dd9d5ff68105
tomekts <tomek_kasperczyk@interia.pl>
2020-11-27 17:14:31 +0100

    raczej koniec

--- js/app.js
+++ js/app.js
@@ -2,81 +2,85 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	var show=1;
-	var first;
-	var second;
-	var firstcard
-	var buttonTest = document.querySelector('.buttonTest');
-	var cardlist = document.querySelectorAll('.card');
-
+	var show=1;// zmienna do kliku pierwsze i drugiego
+	var first; // zmienna do zapisania src pierwszej grafiki
+	var second;// zmienna do zapisania src drugiej grafiki
+	var secodcard // zmienna do zapisania mijesca pierwszego kliku
+	var firstcard// zmienna do zapisania mijesca drugiego kliku
+	var finish=0;//zmienna do zakańczenia gry obłusługuje alert
+	var buttonTest = document.querySelector('.buttonTest');//selektror do przysiku
+	var cardlist = document.querySelectorAll('.card');// selektro do wszystskich pól gry
+	// pierwsze losowanie grafik
+	addRandomImage();
+	//dodanie lisnera na przycisk
 	buttonTest.addEventListener('click', function(){		
 		addRandomImage();
+		finish=0;
 		})
 
-	
+	//dodanie lisnera na pola gry
 	for (var i = 0; i < 16; i++) {
 		cardlist[i].addEventListener('click', check);		
 	}
 
 	
+	//sprawdzenie zaznaczonych dwóch elemntów
+	function check(){			
 
-	function check(){		
-		if (show==1) {
+		if (show==1 || show==3) {
+			unvisibleAll();	
+			show++;
+			
+			this.firstElementChild.classList.remove('unvisible');	
 			first = this.firstElementChild.src;	
 			for (var i =0 ; i<16; i++) {
 				if (this===cardlist[i]) {
 					firstcard=i;
 				}
 			}
-
 			console.log('fir: ' +first)		
 		}			
 		
-		this.firstElementChild.classList.remove('unvisible');
+		
 		if (show==2) {
+			this.firstElementChild.classList.remove('unvisible');
 			second = this.firstElementChild.src;	
-			console.log('sec: '+second)			
-			if (first===second) {
+			console.log('sec: '+second)		
+			for (var i =0 ; i<16; i++) {
+				if (this===cardlist[i]) {
+					secodcard=i;
+				}
+				
+			}	
+			if (first===second && !(firstcard===secodcard)) {
 				console.log('brawo');
 				this.firstElementChild.classList.add('oki');
 				cardlist[firstcard].firstElementChild.classList.add('oki');
-				
-				
+				unvisibleAll();
+				finish++;
+				if (finish==8) {
+				setTimeout(()=>{alert('brawo koniec gry')},200);
+				}
 			}
 		}
-
-		if (show==3) {
-			show=0;
-			for (var i = 0; i <cardlist.length; i++) {
-				cardlist[i].firstElementChild.classList.add('unvisible');
-			}
-		}
-		show++;
 		
+		show++;
+		console.log(finish);
 	}
 
-			
-	
+	// ukrywanie grafik		
+	function unvisibleAll(){
+		show=0;
+		for (var i = 0; i <cardlist.length; i++) {
+				cardlist[i].firstElementChild.classList.add('unvisible');
+			}
+	}
 
-//testowanie js XD	
-/*
-	var first = this.firstElementChild.src;		
-	var second = this.firstElementChild.src;
-	var cars = ['kot', 'pies', 'indyk'];
-	console.log(cars);
-	cars.push('kot');
-	var car=[];
-	car.push('kot');
-	console.log(car.length);
-	
-	console.log(car.includes('kot'));
-*/
-
-	
-
+	//do rozmieszania w losowych miejscach grafik
 	function addRandomImage(){
 		var bussySite=[];
-		var bussyImage=[];	
+		var bussyImage=[];
+		show=1;	
 
 		for (var i =0; bussyImage.length<8; i++){
 					
@@ -99,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			}			
 		}		
 	}
-
+	//do dodawania danej grafiki w dane mijesce
 	function addImage(site, number){
 		var image = document.createElement('img');
 		image.src=("./images/icon"+number+'.png');
@@ -109,5 +113,6 @@ document.addEventListener('DOMContentLoaded', function() {
 		cardlist[site].appendChild(image);	
 		
 	}
+	
 
 });
