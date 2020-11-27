7e1d32f3e01f33ced77271ca090fb388c0df88dc
tomekts <tomek_kasperczyk@interia.pl>
2020-11-27 12:07:44 +0100

    działa losowe rozmieszczanie

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		
 		
 		addRandomImage();
-		
+
 	})
 
 //testowanie js XD	
@@ -17,19 +17,40 @@ document.addEventListener('DOMContentLoaded', function() {
 	// var cars = ['kot', 'pies', 'indyk'];
 	// console.log(cars);
 	// cars.push('kot');
-	//var car=[];
-	//car.push('kot');
-	//console.log(car.length);
+	// var car=[];
+	// car.push('kot');
+	// console.log(car.length);
+	
+	// console.log(car.includes('kot'));
+
 	
 
 	function addRandomImage(){
 		console.log('adrandm');
 		var bussySite=[];
-		var bussyImage=[];
-		
-		
-	
-		}
+		var bussyImage=[];	
+
+		for (var i =0; bussyImage.length<8; i++){
+					
+			var image = Math.floor(Math.random() * 8);
+			var pic=0;
+						
+			if (!bussyImage.includes(image)) {				
+				bussyImage.push(image);
+
+
+				while (pic<2){
+					var site = Math.floor(Math.random() * 16); 
+					//cardlist[1].innerHTML;
+					if (!bussySite.includes(site)) {
+						bussySite.push(site);											
+						addImage(site,image);
+						pic++;
+					}					
+				}
+			}			
+		}		
+	}
 
 	
 	
